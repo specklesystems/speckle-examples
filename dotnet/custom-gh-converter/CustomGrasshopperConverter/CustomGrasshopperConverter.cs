6433fd7e8638fd2d0600a62afa3b2925400275a9
Alan Rynne <alan@speckle.systems>
2022-05-25 10:36:08 +0200

    dotnet: First run at creating a custom Kit.
    Pushing here so the rest of the connectors team can check it out if needed.

---
+++ dotnet/custom-gh-converter/CustomGrasshopperConverter/CustomGrasshopperConverter.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using Speckle.Core;
+using Speckle.Core.Models;
+using Objects.Converter.RhinoGh;
+using Speckle.Core.Kits;
+using CustomSpeckleObjects;
+
+namespace CustomGrasshopperConverter
+{
+
+    public class CustomGrasshopperConverter : ISpeckleConverter
+    {
+        private ConverterRhinoGh BaseConverter = new();
+
+        public string Description => "My custom converter";
+        public string Name => "Custom GH converter";
+        public string Author => "Alan Rynne";
+        public string WebsiteOrEmail => "alan@speckle.systems";
+
+        public ProgressReport Report => BaseConverter.Report;
+
+        public ReceiveMode ReceiveMode { get; set; }
+
+        public bool CanConvertToNative(Base @object)
+        {
+            if (@object is FancyObject)
+            {
+                return true;
+            }
+            return BaseConverter.CanConvertToNative(@object);
+        }
+
+        public bool CanConvertToSpeckle(object @object)
+        {
+            return BaseConverter.CanConvertToSpeckle(@object);
+        }
+
+        public object ConvertToNative(Base @object)
+        {
+            if (@object is FancyObject fancy)
+            {
+                return BaseConverter.ConvertToNative(fancy.Origin);
+            }
+            return BaseConverter.ConvertToNative(@object);
+        }
+
+        public List<object> ConvertToNative(List<Base> objects)
+        {
+            return BaseConverter.ConvertToNative(objects);
+        }
+
+        public Base ConvertToSpeckle(object @object)
+        {
+            return BaseConverter.ConvertToSpeckle(@object);
+        }
+
+        public List<Base> ConvertToSpeckle(List<object> objects)
+        {
+            return BaseConverter.ConvertToSpeckle(objects);
+        }
+
+        public IEnumerable<string> GetServicedApplications()
+        {
+            return BaseConverter.GetServicedApplications();
+        }
+
+        public void SetContextDocument(object doc)
+        {
+            BaseConverter.SetContextDocument(doc);
+        }
+
+        public void SetContextObjects(List<ApplicationPlaceholderObject> objects)
+        {
+            BaseConverter.SetContextObjects(objects);
+        }
+
+        public void SetConverterSettings(object settings)
+        {
+            BaseConverter.SetConverterSettings(settings);
+        }
+
+        public void SetPreviousContextObjects(List<ApplicationPlaceholderObject> objects)
+        {
+            BaseConverter.SetPreviousContextObjects(objects);
+        }
+    }
+}
---
+++ dotnet/custom-gh-converter/CustomSpeckleObjects/CustomObject.cs
@@ -0,0 +1,24 @@
+﻿using Speckle.Core.Models;
+using Objects.Geometry;
+using Speckle.Core.Kits;
+
+namespace CustomSpeckleObjects
+{
+    public class FancyObject : Base
+    {
+        [DetachProperty]
+        public Plane? Origin { get; set; }
+
+        public FancyObject()
+        {
+
+        }
+
+        [SchemaInfo("Fancy Object", "A fancy object based on a plane.")]
+        public FancyObject(Plane plane)
+        {
+            Origin = plane;
+        }
+    }
+
+}
